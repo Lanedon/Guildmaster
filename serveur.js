ed3ea0d8599e487c7e07db2c399ca1f9039b7d47
tdalex <dalex.tanguy@gmail.com>
2016-04-28 14:32:15 +0200

    ajout style

--- serveur.js
+++ serveur.js
@@ -6,7 +6,7 @@ var mysql       = require('mysql');
 var connection = mysql.createConnection({
   host     : "localhost",
   user     : "root",
-  password : "",
+  password : "sio",
   database : "guildmaster"
 });
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
